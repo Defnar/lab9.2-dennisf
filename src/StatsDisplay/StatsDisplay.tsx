addff9d640c025e20e16438e9315d02116b98c0a
Dennis <dennis-farris@hotmail.com>
2025-07-24 17:43:26 -0400

    stat display component finished

--- src/StatsDisplay/StatsDisplay.tsx
+++ src/StatsDisplay/StatsDisplay.tsx
@@ -1,5 +1,22 @@
-import type { StatsDisplayProps, TextStats } from "../types";
+import type { StatsDisplayProps } from "../types";
+import { formattedReadingTime } from "../utils/statCalculator";
 
-export default function StatsDisplay({stats, showReadingTime}:StatsDisplayProps) {
-
+export default function StatsDisplay({
+  stats,
+  showReadingTime = true,
+}: StatsDisplayProps) {
+  return (
+    <>
+      <div>
+        <p>Characters</p>
+        <p>{stats.characterCount}</p>
+      </div>
+      <p>Words</p>
+      <p>{stats.wordCount}</p>
+      <div>
+        <p>Reading Time</p>
+        <p>{showReadingTime ? formattedReadingTime(stats.readingTime) : ""}</p>
+      </div>
+    </>
+  );
 }
