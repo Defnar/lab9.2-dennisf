3348f60ebaff42fcf83091fce3c34329b7dc1b55
Dennis <dennis-farris@hotmail.com>
2025-07-24 16:46:48 -0400

    text input component created

---
+++ src/TextInput/TextInput.tsx
@@ -0,0 +1,16 @@
+import type { TextInputProps } from "../types";
+
+export default function TextInput({onTextChange, placeholder = "start typing...", initialValue = ""}: TextInputProps) {
+
+    const handleTextChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
+        onTextChange(event.target.value);
+    }
+
+    return (
+        <textarea 
+        placeholder={placeholder}
+        value={initialValue}
+        onChange={handleTextChange}
+        />
+    )
+}
